880cad1b4a12495b8e59b08da870dc031658df66
mdunggggg <dungmanh74@gmail.com>
2025-03-16 17:50:05 +0700

    Initial Commit

---
+++ map_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  map_app
+//
+//  Created by ImDung on 16/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ map_app/map_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_appApp.swift
+//  map_app
+//
+//  Created by ImDung on 16/3/25.
+//
+
+import SwiftUI
+
+@main
+struct map_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
